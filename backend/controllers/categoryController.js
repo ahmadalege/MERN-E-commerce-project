5d35ab5dd5156b5da270beda06be678438b1d53e
ahmadalege <131969880+ahmadalege@users.noreply.github.com>
2025-03-21 14:57:18 +0100

    Category Management

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,14 +1,83 @@
 import Category from "../models/categoryModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
-const createCaetgory = asyncHandler(async (req, res) => {
+const createCategory = asyncHandler(async (req, res) => {
   try {
     const { name } = req.body;
-    console.log(name);
+
+    if (!name) {
+      return res.json({ error: "Name is required" });
+    }
+
+    const existingCategory = await Category.findOne({ name });
+
+    if (existingCategory) {
+      return res.json({ error: "Category already exists" });
+    }
+
+    const category = await new Category({ name }).save();
+    res.json(category);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(400).json(error);
   }
 });
 
-export { createCaetgory };
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    category.name = name;
+
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const deleted = await Category.findByIdAndDelete(req.params.categoryId);
+    res.json(deleted);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+const getCategories = asyncHandler(async (req, res) => {
+  try {
+    const all = await Category.find({});
+    res.json(all);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+const readCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.findOne({ _id: req.params.id });
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json(error.message);
+  }
+});
+
+export {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories,
+  readCategory,
+};
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware";
+import {
+  authenticate,
+  authorizedAdmin,
+} from "../middlewares/authMiddleware.js";
+import {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories,
+  readCategory,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, authorizedAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizedAdmin, updateCategory);
+router
+  .route("/:categoryId")
+  .delete(authenticate, authorizedAdmin, deleteCategory);
+router.route("/categories").get(getCategories);
+router.route("/:id").get(readCategory);
 
 export default router;
